f6e5dfb2e561945a555c8388a6d421e6052205f6
Max <xthatonekid@gmail.com>
2020-12-08 22:53:35 -0800

    Simple document selector proof of concept

--- styles/global.js
+++ styles/global.js
@@ -1,17 +1,22 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const globalStyles = StyleSheet.create({
-    container:{
-        flex: 1,
-        padding: 20
-    },
-    titleText:{
-        fontFamily: 'nunito-bold',
-        fontSize: 18,
-        color: '#333'
-    },
-    paragraph:{
-        marginVertical: 8,
-        lineHeight: 20
-    }
-  });
+  container: {
+    margin: 50,
+    flex: 1,
+    padding: 20,
+  },
+  titleText: {
+    fontFamily: "nunito-bold",
+    fontSize: 18,
+    color: "#333",
+  },
+  paragraph: {
+    marginVertical: 8,
+    lineHeight: 20,
+  },
+  image: {
+    width: 50,
+    height: 50,
+  },
+});
