2dc396104ef5f64568acecaef4182a7d5db56a80
Ppjamies <Pazuzujindrich@gmail.com>
2020-12-10 11:57:19 -0800

    organized files

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 //import * as Font from 'expo-font';
 //import { AppLoading } from 'expo';
 import Navigator from "./routes/HomeStack";
-import CameraVideo from "./screens/CameraVideo";
-import DocumentPickerTest from "./screens/DocumentPickerTest";
+import CameraVideo from "./screens/experimenting/CameraVideo";
+import DocumentPickerTest from "./screens/experimenting/DocumentPickerTest";
 
 export default function App() {
   return (
     //<Navigator/>
     //  <CameraVideo/>
-    <DocumentPickerTest />
+    <DocumentPickerTest/>
   );
 }
